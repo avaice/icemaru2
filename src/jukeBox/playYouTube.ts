525f5d7379f9c263eea89ad1c7f863ff845fcd43
avaice <dev@cho-ice.xyz>
2023-06-22 22:58:49 +0900

    たまに再生エラーが出るのを改善（できたか？）

--- src/jukeBox/playYouTube.ts
+++ src/jukeBox/playYouTube.ts
@@ -3,20 +3,24 @@ import { AudioPlayerStatus, createAudioPlayer, createAudioResource, VoiceConnect
 import ytdl from "ytdl-core"
 import { playNext } from "./checkSkipCommand"
 export const playYouTube = (url: string, message: Message, connection: VoiceConnection, channel: VoiceBasedChannel) => {
-  // https://github.com/fent/node-ytdl-core/issues/902
-  const stream = ytdl(ytdl.getURLVideoID(url), {
-    filter: "audioonly",
-    highWaterMark: 1 << 62,
-    liveBuffer: 1 << 62
-  })
-  const player = createAudioPlayer()
-  const resource = createAudioResource(stream)
-  player.play(resource)
-  resource.volume?.setVolume(0.5)
+  try {
+    // https://github.com/fent/node-ytdl-core/issues/902
+    const stream = ytdl(ytdl.getURLVideoID(url), {
+      filter: "audioonly",
+      highWaterMark: 1 << 62,
+      liveBuffer: 1 << 62
+    })
+    const player = createAudioPlayer()
+    const resource = createAudioResource(stream)
+    player.play(resource)
+    resource.volume?.setVolume(0.5)
 
-  player.on(AudioPlayerStatus.Idle, () => {
-    playNext(message, connection, channel)
-  })
+    player.on(AudioPlayerStatus.Idle, () => {
+      playNext(message, connection, channel)
+    })
 
-  connection.subscribe(player)
+    connection.subscribe(player)
+  } catch {
+    message.reply(`${url}\n↑の曲、歌おうと思ったけどうまくいかなかった...`)
+  }
 }
